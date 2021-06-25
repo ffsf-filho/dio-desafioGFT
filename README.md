d564681abe6c9c9d91238e9b01b6b05de3707558
Francisco Florentino de Sousa Filho <ffsf.filho@gmail.com>
2021-06-25 20:13:22 -0300

    Inclusão do primeiro desafio DividindoXPorY

---
+++ README.md
@@ -0,0 +1,29 @@
+# Bootcamp GFT START #2 .NET
+
+## Projeto contendo a resolução de vários desafios propostos no Bootcamp
+
+Este projeto foi desenvolvido durante o Bootcamp.
+
+### Requisitos
+
+* Windows 7 ou superior
+* .NET versão 5.0.103
+
+### Instruções
+
+1. Fazer Downloading do projeto
+  <a href="https://github.com/ffsf-filho/dio-desafioGFT/archive/main.zip">Download ZIP</a>
+    
+    OU
+    
+    ```git clone  https://github.com/ffsf-filho/dio-desafioGFT.git```
+2. Descompactar o projeto
+3. Acessar o prompt de comando, para ir até a paste do projeto
+4. Executar o comando de build do dotnet
+   ```
+   dotnet build
+   ```
+5. Executar o programa
+   ```
+   dotnet run
+   ```
---
+++ dio-DesafioGFT/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace dio_DesafioGFT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DividindoXPorY();
+        }
+
+        protected static void DividindoXPorY()
+        {
+            //Desafio
+            //Você terá o desafio de escrever um algoritmo que leia 2 números e imprima o resultado da divisão do primeiro pelo segundo.
+            //Caso não for possível, mostre a mensagem “divisao impossivel” para os valores em questão.
+
+            //Entrada
+            //A entrada contém um número inteiro N.Este N será a quantidade de pares de valores inteiros(X e Y) que serão lidos em seguida.
+
+            //Saída
+            //Para cada caso mostre o resultado da divisão com um dígito após o ponto decimal, ou “divisao impossivel” caso não seja
+            //possível efetuar o cálculo.
+
+            int limit = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < limit; i++)
+            {
+                string[] line = Console.ReadLine().Split(" ");
+                double X = double.Parse(line[0]);
+                double Y = line.Length > 1 ? double.Parse(line[1]) : 0;
+                if (Y == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    double divisao = X / Y; // Digite aqui o calculo da divisao
+                    Console.WriteLine(divisao.ToString("N1"));
+                }
+            }
+        }
+    }
+}
